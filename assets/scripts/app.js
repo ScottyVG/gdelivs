d8412c79992222a0464f7118373fbfc82d48588b
Scott Van Gilder <svangilder@gmail.com>
2016-08-19 15:42:48 -0600

    footer work

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,6 +15,25 @@ $(document).ready(function() {
     $('#cart').append(`<tr><td>${title}</td>
     <td>${price}</td></tr>`)
     console.log(`Price: ${price} Title:${title}`);
+    updateTotal(price);
+  }
+  var updateTotal = function(price) {
+    let price_value = getValue(price)
+    let current_subtotal = getValue($('#subtotal').text())
+    let current_tax = getValue($('#tax').text())
+    let current_total = getValue($('#total').text())
+    current_subtotal += price_value
+    current_tax = current_subtotal * 0.085
+    current_total = current_subtotal + current_tax
+
+    console.log(`current total= ${current_total}`);
+
+    $('#subtotal').text('$' + current_subtotal.toFixed(2))
+    $('#tax').text('$' + current_tax.toFixed(2))
+    $('#total').text('$' + current_total.toFixed(2))
+  }
+  var getValue = function(value) {
+    return parseFloat(value.replace(/\$/, ""))
   }
 
 
